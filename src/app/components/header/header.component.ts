150f50ee3c480b410fbb0cbd0d7e5842d74f78cf
pedro freire <pedrofc31.31@gmail.com>
2025-03-21 07:22:23 -0300

    adding tailwind

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="header">
+      {{title()}}
+    </div>
+  `,
+  styles: `
+
+    .header {
+      background: rgba(0,0,0, .5);
+      color: black;
+      padding: 1rem;
+    }
+  
+  `
+})
+export class HeaderComponent {
+  title = signal('signal haehe')
+
+}
